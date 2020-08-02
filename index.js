40be20eb3631831d1d64bd9f62f0ed0fd0e66ae2
Nico <nicolascagiao@hotmail.com>
2020-08-02 16:19:47 -0300

    nodemailer

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
 const Datastore = require('nedb');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars')
 const nodemailer = require('nodemailer');
 
-
-
 app.listen((process.env.PORT || 5000), function(){
-    console.log('listening on *:5000');
-  });
+  console.log('listening on *:5000');
+});
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
-
 app.use(express.json({limit:'1mb'}));
  
-
 const lDatabase = new Datastore('LDatabase.db');
 lDatabase.loadDatabase();
-
 const pDatabase = new Datastore('pDatabase.db');
 pDatabase.loadDatabase();
-
 
 app.get('/abecedarium', abcLearn);
 
@@ -75,3 +77,42 @@ app.post('/api1', (request, response) => {
         avatar: data.avatarUrl
     });
 });
+
+app.post('/send', (request, response)=> {
+  const output = `
+    <p>Nuevo Mensaje desde nicolascagiao.com</p>
+     <ul>  
+      <li>Nombre: ${request.body.nombre}</li>
+      <li>Email: ${request.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${request.body.mensaje}</p>
+  `;
+   
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "nico.cagiao@gmail.com", 
+        pass:"rfqmglxdogykcntf",
+      },
+    });
+  let mailOptions = {
+    from: 'nicolascagiao.com',
+    to: "nico.cagiao@gmail.com",
+    subject: "Nuevo mensaje desde el sitio",
+    html: output,
+  };
+
+  transporter.sendMail(mailOptions,(error, info) =>{
+    if(error){
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    response.render({msg: 'Email enviado!'});
+  });    
+ });
+
+
+
